18b2e2e8432bbc8e34806de74182479f769ef981
Vagrant User <vagrant@vagrant-arch.vagrantup.com>
2016-02-22 06:27:34 +0900

    typo

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import yaml
 
@@ -11,7 +12,6 @@ def generate_build_task(destination="test"):
         for y in TASK.get('Required'):
             f.write('cp -rf {} {}\n'.format(y, DESTINATION))
         f.write ('cd {}\n'.format(DESTINATION))
-
         project_name = yml['Projects']['Name'][0]
         sub_name = '/{}'.format(yml['Projects']['SubName'][0]) if yml['Projects']['SubName'][0] else ''
         version = ':' + yml['Projects']['Version'][0] if yml['Projects'].get('Version','') else ''
@@ -22,17 +22,16 @@ def generate_build_task(destination="test"):
                                                                     version=version)
         build_string = 'docker build -t {project_name} .\n'.format(project_name=project_name)
         f.write (build_string)
-        #print (TASK['Volume'])
         volume = "mkdir {}".format(TASK['Volume'][0]) if TASK['Volume'][0] else ''
         f.write(volume)
         f.write('\n')
         if volume:
-            volume_path = "-v `pwd`/{}/tmp".format(volume)
+            #os.chdir()
+            volume_path = "-v {}".format(os.getcwd() + os.sep + DESTINATION)
         else:
             volume_path = ""
         run_string = 'docker run {volume_path} {project_name}\n'.format(volume_path=volume_path,
                                                                         project_name=project_name)
-        #print (run_string)
         f.write (run_string)
 
 if __name__ == '__main__':
