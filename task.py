1b4dc230c282d05513ab36377bd8cd5a5f9952f5
Vagrant User <vagrant@vagrant-arch.vagrantup.com>
2016-02-22 06:18:10 +0900

    typo

--- task.py
+++ task.py
@@ -20,16 +20,20 @@ def generate_build_task(destination="test"):
         project_name = '{project_name}{sub_name}{version}'.format(project_name=project_name,
                                                                     sub_name=sub_name,
                                                                     version=version)
-        f.write ('docker build -t {project_name} .\n'.format(project_name=project_name))
-        volume = "mkdir {}\n.format".format(TASK['Volume'][0]) if TASK['Volume'][0] else ''
+        build_string = 'docker build -t {project_name} .\n'.format(project_name=project_name)
+        f.write (build_string)
+        #print (TASK['Volume'])
+        volume = "mkdir {}".format(TASK['Volume'][0]) if TASK['Volume'][0] else ''
+        f.write(volume)
+        f.write('\n')
         if volume:
             volume_path = "-v `pwd`/{}/tmp".format(volume)
         else:
             volume_path = ""
-        f.write ('docker run {volume_path} {project_name}\n'.format(volume_path=volume_path,
-                                                                 project_name=project_name))
-
-
+        run_string = 'docker run {volume_path} {project_name}\n'.format(volume_path=volume_path,
+                                                                        project_name=project_name)
+        #print (run_string)
+        f.write (run_string)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
